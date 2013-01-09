2cb502c3e63088ed2ec1ca781717e476e110c798
Mark Stahl <mjstahl@gmail.com>
2013-01-09 04:01:18 +0000

    Did not stage the rest of the changes.  Restating.
    Changed the command "version" to "info".  Currently it only prints out the language version number (which is arbitrary at this point).
    Added the commands "add-broker", "console", "get", and "use" to the usage statement, but have yet to implement them.
    Completed the implementation of the "create" command.
    Anytime a user uses the "disco" command, a check occurs for the ".disco.root" directory in the user's home. If it doesn't exist it creates it.

--- src/disco/disco.go
+++ src/disco/disco.go
@@ -4,11 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/user"
 )
 
 var version = "0.1.0"
 
 func main() {
+	if discoRootDoesNotExist() {
+		createDiscoRoot()
+	}
+
 	flag.Parse()
 
 	args := flag.Args()
@@ -20,9 +25,32 @@ func main() {
 	default:
 		unknownCommand(args[0])
 	case "create":
-		createProject(args[1:])
-	case "version":
-		printVersion()
+		createProjectDir(args[1:])
+	case "info":
+		printRuntimeInfo()
+	}
+}
+
+func discoRootDoesNotExist() bool {
+	user, _ := user.Current()
+	rootDir := user.HomeDir + "/.disco.root"
+
+	if _, err := os.Stat(rootDir); err != nil {
+		if os.IsNotExist(err) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func createDiscoRoot() {
+	user, _ := user.Current()
+
+	rootDir := user.HomeDir + "/.disco.root"
+	err := os.Mkdir(rootDir, 0700)
+	if err != nil {
+		fmt.Printf("error creating ~/.disco.root: %s", err)
 	}
 }
 
@@ -31,26 +59,74 @@ func unknownCommand(cmd string) {
 	printUsage()
 }
 
-var usageText = 
-`Usage: 
+var discoCmdUsageText = `Usage: 
     disco [command] [arguments]
 
 The commands are:
-    create		create a discourse project
-    version		print the discourse version
+    - add-broker	add broker to discourse project or root
+    - console		interact with the discourse runtime
+    + create		create a discourse project
+    - get		retrieve a discourse archive
+    + info		display discourse runtime information
+    - use		retrieve a discourse manifest
+
+Commands marked with '+' are ready for use.
 `
 
 func printUsage() {
-	fmt.Println(usageText)
+	fmt.Println(discoCmdUsageText)
 	os.Exit(0)
 }
 
-func createProject(args []string) {
-	cwd, _ := os.Getwd()
-	fmt.Printf("creating '%s' project at '%s'\n", args[0], cwd)
+func createProjectDir(args []string) {
+	if len(args) < 1 {
+		// should display the information for
+		// 'disco create' not the 'disco' command
+		// usage
+		printUsage()
+	}
+
+	projName := args[0]
+	pwd, _ := os.Getwd()
+
+	fmt.Printf("    create %s/\n", projName)
+
+	projDir := pwd + "/" + projName
+	err := os.Mkdir(projDir, 0700)
+	if err != nil {
+		fmt.Printf("error creating project directory: %s\n", err)
+		os.Exit(0)
+	}
+
+	fmt.Printf("    create %s/%s\n", projName, ".disco.proj")
+
+	projFile := pwd + "/" + projName + "/.disco.proj"
+	file, err := os.OpenFile(projFile, os.O_CREATE, 0700)
+	if err != nil {
+		fmt.Printf("error creating project root: %s\n", err)
+		os.Exit(0)
+	}
+
+	file.Close()
+
+	fmt.Printf("    create %s/%s\n", projName, "dist")
+
+	projDistDir := projDir + "/dist"
+	err = os.Mkdir(projDistDir, 0700)
+	if err != nil {
+		fmt.Printf("error creating project distribution directory: %s\n", err)
+		os.Exit(0)
+	}
+
+	fmt.Printf("    create %s/%s\n", projName, "src")
+
+	projSrcDir := projDir + "/src"
+	err = os.Mkdir(projSrcDir, 0700)
+	if err != nil {
+		fmt.Printf("error creating project source directory: %s\n", err)
+	}
 }
 
-func printVersion() {
-	fmt.Printf("discourse version %s\n", version)
+func printRuntimeInfo() {
+	fmt.Printf("Discourse version %s\n", version)
 }
-
