8c97b2d83bc0d2ecafbfd9e1cd090224185e53a2
mjstahl@gmail.com <mjstahl@gmail.com>
2013-01-13 15:00:37 -0500

    Moved the top level project creation functionality (arg check, usage, etc) to the cmd/create.go file.

--- src/disco/cmd/create.go
+++ src/disco/cmd/create.go
@@ -14,3 +14,40 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package cmd
+
+import (
+    "disco/file"
+    "fmt"
+    "os"
+)
+
+var CreateUsage = 'Usage:
+    disco create <project name>
+    
+    Creates a discourse project directory as a
+    subdirectory of the current directory.
+
+Example:
+    $ disco create Boolean
+        created Boolean
+        created Boolean/.disco.proj
+        created Boolean/dist
+        created Boolean/src
+        created Boolean/src/Boolean.disco
+'
+
+func CreateProject(args []string) {
+    if len(args) < 1 {
+        DisplayCreateError()
+        os.Exit(0)
+    }
+    
+    projName := args[0]
+    pwd, _ := os.Getwd()
+    file.CreateProject(projName, pwd)
+}
+
+func DisplayCreateError() {
+    fmt.Printf("disco create: unknown argument(s)\n")
+    fmt.Printf("%s\n", CreateUsage)
+}
