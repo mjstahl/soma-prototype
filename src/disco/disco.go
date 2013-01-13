001ca5d3f1c42845b3f984caa0e74428a5ffade3
Mark Stahl <mjstahl@gmail.com>
2013-01-13 03:58:05 +0000

    Added the 'help' and 'scan' commands to the 'disco' command.  This file will go through a lot of refactoring over the next few days as I figure out how I want to structure the project.

--- src/disco/disco.go
+++ src/disco/disco.go
@@ -1,12 +1,27 @@
 // Copyright (C) 2013 Mark Stahl
 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import (
+	"disco/cmd"
 	"flag"
 	"fmt"
 	"os"
 	"os/user"
+	"path"
 )
 
 var version = "0.1.0"
@@ -27,15 +42,24 @@ func main() {
 	default:
 		unknownCommand(args[0])
 	case "create":
+		// cmd.CreateProject(args[1:])
 		createProjectDir(args[1:])
+	case "help":
+		if len(args) < 2 {
+			printUsage()
+		}
+		cmd.CommandHelp(args[1])
 	case "info":
+		// cmd.RuntimeInfo()
 		printRuntimeInfo()
+	case "scan":
+		cmd.Scan(args[1:])
 	}
 }
 
 func discoRootDoesNotExist() bool {
 	user, _ := user.Current()
-	rootDir := user.HomeDir + "/.disco.root"
+	rootDir := path.Join(user.HomeDir, "/.disco.root")
 
 	if _, err := os.Stat(rootDir); err != nil {
 		if os.IsNotExist(err) {
@@ -49,7 +73,7 @@ func discoRootDoesNotExist() bool {
 func createDiscoRoot() {
 	user, _ := user.Current()
 
-	rootDir := user.HomeDir + "/.disco.root"
+	rootDir := path.Join(user.HomeDir, "/.disco.root")
 	err := os.Mkdir(rootDir, 0700)
 	if err != nil {
 		fmt.Printf("error creating ~/.disco.root: %s", err)
@@ -69,7 +93,9 @@ The commands are:
     - console		interact with the discourse runtime
     + create		create a discourse project
     - get		retrieve a discourse archive
+    - help		display help for a disco command
     + info		display discourse runtime information
+    - scan		lexically analyze discourse source
     - serve		serve a project to peers
     - use		retrieve a discourse manifest
 
@@ -94,27 +120,26 @@ func createProjectDir(args []string) {
 
 	fmt.Printf("    create %s/\n", projName)
 
-	projDir := pwd + "/" + projName
+	projDir := path.Join(pwd, projName)
 	err := os.Mkdir(projDir, 0700)
 	if err != nil {
 		fmt.Printf("error creating project directory: %s\n", err)
 		os.Exit(0)
 	}
 
-	projFileName := pwd + "/" + projName + "/.disco.proj"
+	projFileName := path.Join(pwd, projName, ".disco.proj")
 	fmt.Printf("    create %s/%s\n", projName, ".disco.proj")
 
 	file, err := os.OpenFile(projFileName, os.O_CREATE, 0700)
+	defer file.Close()
 	if err != nil {
 		fmt.Printf("error creating project root: %s\n", err)
 		os.Exit(0)
 	}
 
-	file.Close()
-
 	fmt.Printf("    create %s/%s\n", projName, "dist")
 
-	projDistDir := projDir + "/dist"
+	projDistDir := path.Join(projDir, "dist")
 	err = os.Mkdir(projDistDir, 0700)
 	if err != nil {
 		fmt.Printf("error creating project distribution directory: %s\n", err)
@@ -123,7 +148,7 @@ func createProjectDir(args []string) {
 
 	fmt.Printf("    create %s/%s\n", projName, "src")
 
-	projSrcDir := projDir + "/src"
+	projSrcDir := path.Join(projDir, "src")
 	err = os.Mkdir(projSrcDir, 0700)
 	if err != nil {
 		fmt.Printf("error creating project source directory: %s\n", err)
@@ -132,14 +157,13 @@ func createProjectDir(args []string) {
 
 	fmt.Printf("    create %s/%s/%s\n", projName, "src", projName+".disco")
 
-	projMainFile := projSrcDir + "/" + projName + ".disco"
+	projMainFile := path.Join(projSrcDir, projName+".disco")
 	mainFile, err := os.OpenFile(projMainFile, os.O_CREATE, 0700)
+	defer mainFile.Close()
 	if err != nil {
 		fmt.Printf("error creating main project file: %s\n", err)
 		os.Exit(0)
 	}
-
-	mainFile.Close()
 }
 
 func printRuntimeInfo() {
