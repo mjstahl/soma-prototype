bbc095282bc14857441726898fa2960d2d8ed383
Mark Stahl <mjstahl@gmail.com>
2013-01-27 18:46:45 +0000

    Removed the default case for a more specific error case.  This will most likely change as we get more tokens into the language. Also added the case for when an IDENT comes after a literal (a unary expression).

--- src/disco/parse/lit.go
+++ src/disco/parse/lit.go
@@ -15,12 +15,13 @@ func (p *Parser) parseLiteral(l *ast.Literal) ast.Expression {
 	var expr ast.Expression = l
 	for p.tok != scan.PERIOD {
 		switch {
-		default:
+		case p.tok == scan.LBRACK || p.tok == scan.NAME:
 			return nil // error case
-		case p.tok == scan.IDENT:
-			expr = &ast.Expr{Receiver: expr, Behavior: p.lit}
 		case p.tok == scan.BINARY:
 
+		case p.tok == scan.IDENT:
+			expr = &ast.Expr{Receiver: expr, Behavior: p.lit}
+		
 		case p.tok == scan.KEYWORD:
 
 		}
