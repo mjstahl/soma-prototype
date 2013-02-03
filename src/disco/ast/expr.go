05b2dfd50ff19d0f511d07fc7d5a1a32b9f09b76
Mark Stahl <mjstahl@gmail.com>
2013-02-03 18:49:59 +0000

    Removed the Message interface as it wasn't needed.

--- src/disco/ast/expr.go
+++ src/disco/ast/expr.go
@@ -8,18 +8,9 @@ type Expr interface {
 	Visit()
 }
 
-type Message interface {
-	Expr
-	SetReceiver(recv Expr)
-}
-
 type UnaryMessage struct {
 	Receiver Expr
 	Behavior string
-}
-
-func (ue *UnaryMessage) SetReceiver(recv Expr) {
-	ue.Receiver = recv
 }
 
 func (ue *UnaryMessage) Visit() {}
@@ -30,20 +21,12 @@ type BinaryMessage struct {
 	Arg      Expr
 }
 
-func (be *BinaryMessage) SetReceiver(recv Expr) {
-	be.Receiver = recv
-}
-
 func (be *BinaryMessage) Visit() {}
 
 type KeywordMessage struct {
 	Receiver Expr
 	Behavior string
 	Args     []Expr
-}
-
-func (ke *KeywordMessage) SetReceiver(recv Expr) {
-	ke.Receiver = recv
 }
 
 func (ke *KeywordMessage) Visit() {}
