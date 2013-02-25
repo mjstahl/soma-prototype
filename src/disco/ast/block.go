199fbb4b1d5653046ed9eba1c0fd14d1e81f7714
Mark Stahl <mjstahl@gmail.com>
2013-02-25 15:18:54 -0500

    Updated #22.  What happens when a block has no exprs to evaluate.  It should return Nil, but nil is not a primitive in the system (as of yet).

--- src/disco/ast/block.go
+++ src/disco/ast/block.go
@@ -33,6 +33,10 @@ func (b *Block) Eval(s *rt.Scope) (rt.Value, error) {
 
 		values = append(values, val)
 	}
-
+	
+	// TODO: Issue #22
+	// this will cause a panic/stack trace when the block is
+	// is empty.  need a null check here to at least stop
+	// the panic, but what do we do when it is empty???
 	return values[len(values)-1], nil
 }
