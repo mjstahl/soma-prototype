646b42e737a1b4a29a412b7ec1399e58b65789db
Mark Stahl <mjstahl@gmail.com>
2013-02-28 11:45:28 -0500

    Fixed #42.  Replaced specific creation methods with just two methods to create all of the project directories and files.

--- src/disco/file/project.go
+++ src/disco/file/project.go
@@ -21,81 +21,54 @@ import (
 	"path"
 )
 
-func CreateProject(name string, pwd string) {
-	projDir := path.Join(pwd, name)
-	createProjDir(projDir)
-	fmt.Printf("    created %s/\n", name)
+func CreateProjectDir(name string, pwd string) {
+	fs := createProjectFS(pwd, name)
 
-	projFileName := path.Join(pwd, name, ".disco")
-	createProjFile(projFileName)
-	fmt.Printf("    created %s/%s\n", name, ".disco")
-
-	projDocDir := path.Join(projDir, "doc")
-	createDocDir(projDocDir)
-	fmt.Printf("    created %s/%s\n", name, "doc")
-
-	projLibDir := path.Join(projDir, "lib")
-	createLibDir(projLibDir)
-	fmt.Printf("    created %s/%s\n", name, "lib")
-
-	projSrcDir := path.Join(projDir, "src")
-	createSrcDir(projSrcDir)
-	fmt.Printf("    created %s/%s\n", name, "src")
-
-	projMainFile := path.Join(projSrcDir, name+".disco")
-	createProjMainFile(projMainFile)
-	fmt.Printf("    created %s/%s/%s\n", name, "src", name+".disco")
+	createProjectDir(fs, name, "doc")
+	createProjectDir(fs, name, "lib")
+	createProjectDir(fs, name, "src")
+	
+	pfile := fmt.Sprintf("%s/%s.disco", "src", name) 
+	createProjectFile(fs, name, pfile) 
 }
 
-func createProjDir(dir string) {
+func createProjectFS(pwd string, name string) (dir string) {
+	dir = path.Join(pwd, name)
+
 	err := os.Mkdir(dir, 0700)
 	if err != nil {
 		fmt.Printf("disco create: error creating project directory: %s\n", err)
 		os.Exit(0)
 	}
+	
+	fmt.Printf("    created %s/\n", name)
+	createProjectDir(dir, name, ".disco")
+	
+	return
 }
 
-func createProjFile(name string) {
-	file, err := os.OpenFile(name, os.O_CREATE, 0700)
+func createProjectDir(pdir string, pname string, dname string) {
+	dir := path.Join(pdir, dname)
+	err := os.Mkdir(dir, 0700)
+	
+	if err != nil {
+		fmt.Printf("disco create: error creating project directory '%s': %s\n", dname, err)
+		os.Exit(0)
+	}
+	
+	fmt.Printf("    created %s/%s\n", pname, dname)
+}
+
+func createProjectFile(pdir string, pname string, fname string) {
+	f := path.Join(pdir, fname)
+	file, err := os.OpenFile(f, os.O_CREATE, 0700)
 
 	defer file.Close()
 
 	if err != nil {
-		fmt.Printf("disco create: error creating project root: %s\n", err)
+		fmt.Printf("disco create: error creating project file '%s': %s\n", fname, err)
 		os.Exit(0)
 	}
-}
-
-func createLibDir(dir string) {
-	err := os.Mkdir(dir, 0700)
-	if err != nil {
-		fmt.Printf("disco create: error creating project lib directory: %s\n", err)
-		os.Exit(0)
-	}
-}
-
-func createDocDir(dir string) {
-	err := os.Mkdir(dir, 0700)
-	if err != nil {
-		fmt.Printf("disco create: error creating project doc directory: %s\n", err)
-	}
-}
-
-func createSrcDir(dir string) {
-	err := os.Mkdir(dir, 0700)
-	if err != nil {
-		fmt.Printf("disco create: error creating project src directory: %s\n", err)
-		os.Exit(0)
-	}
-}
-
-func createProjMainFile(file string) {
-	mainFile, err := os.OpenFile(file, os.O_CREATE, 0700)
-
-	defer mainFile.Close()
-
-	if err != nil {
-		fmt.Printf("error creating main project file: %s\n", err)
-		os.Exit(0)
-	}
+	
+	fmt.Printf("    created %s/%s\n", pname, fname)
 }
