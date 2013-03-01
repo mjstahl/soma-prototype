cb35e0a22d6a80be177df24149b8e2c109950067
Mark Stahl <mjstahl@gmail.com>
2013-03-01 18:59:53 +0000

    Moved the localIP to the LocalIP function in net.go

---
+++ src/disco/rt/net.go
@@ -0,0 +1,49 @@
+// Copyright (C) 2013 Mark Stahl
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package rt
+
+import (
+	"errors"
+	"net"
+)
+
+func LocalIP() (net.IP, error) {
+	tt, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, t := range tt {
+		aa, err := t.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, a := range aa {
+			ipnet, ok := a.(*net.IPNet)
+			if !ok {
+				continue
+			}
+			v4 := ipnet.IP.To4()
+			if v4 == nil || v4[0] == 127 { // loopback address
+				continue
+			}
+			return v4, nil
+		}
+	}
+
+	return nil, errors.New("cannot find local IP address")
+
+}
--- src/disco/rt/runtime.go
+++ src/disco/rt/runtime.go
@@ -16,7 +16,6 @@
 package rt
 
 import (
-	"errors"
 	"math/rand"
 	"net"
 	"runtime"
@@ -56,7 +55,7 @@ func InitRuntime() *Runtime {
 	procs := runtime.NumCPU()
 	runtime.GOMAXPROCS(procs)
 
-	ipAddr, _ := localIP()
+	ipAddr, _ := LocalIP()
 
 	rtid := 0 | uint64(rand.Uint32())<<31
 
@@ -79,34 +78,6 @@ func NewID(t uint64) (oid uint64) {
 	}
 
 	return oid
-}
-
-func localIP() (net.IP, error) {
-	tt, err := net.Interfaces()
-	if err != nil {
-		return nil, err
-	}
-
-	for _, t := range tt {
-		aa, err := t.Addrs()
-		if err != nil {
-			return nil, err
-		}
-		for _, a := range aa {
-			ipnet, ok := a.(*net.IPNet)
-			if !ok {
-				continue
-			}
-			v4 := ipnet.IP.To4()
-			if v4 == nil || v4[0] == 127 { // loopback address
-				continue
-			}
-			return v4, nil
-		}
-	}
-
-	return nil, errors.New("cannot find local IP address")
-
 }
 
 func init() {
