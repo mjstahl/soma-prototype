a7e0a9c35ed9c8b3fae42f1ac1660ce432fe2580
Mark Stahl <mjstahl@gmail.com>
2013-03-03 22:13:01 +0000

    Started the get cmd. Encryption keys are being created and written to broker directory of a project.

--- src/disco/cmd/get.go
+++ src/disco/cmd/get.go
@@ -16,8 +16,13 @@
 package cmd
 
 import (
+	"disco/crypt"
+	"disco/file"
 	"fmt"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
 var GetUsage = `Usage:
@@ -40,19 +45,96 @@ Example (within the Test project):
     $ disco get https://example.com/1/Nil.dm
         created .disco/brokers/example.com
         written .disco/brokers/example.com/pub.key
-        written .disco/brokers/example.com/priv.key
+        written .disco/brokers/example.com/prv.key
         created user @ https://example.com
         written lib/Nil.dm
 `
 
 func Get(args []string) {
 	if len(args) < 1 {
-		DisplayGetError()
+		fmt.Println("disco get: missing library url")
+		fmt.Println(GetUsage)
 		os.Exit(1)
+	}
+
+	bdir := brokerDirName(args[0]) 
+
+	pwd, _ := os.Getwd()
+	proj := file.ProjDirFrom(pwd)
+	if proj == "" {
+		createRootBrokerDir(bdir)
+	} else {
+		p := path.Join(proj, ".disco/brokers")
+		createProjBrokerDir(p, bdir)
 	}
 }
 
-func DisplayGetError() {
-	fmt.Println("disco use: missing manifest url")
-	fmt.Printf("%s\n", GetUsage)
+func brokerDirName(url string) string {
+	domain := strings.Split(url, "/")
+	d := strings.Split(domain[2], ".")
+
+	broker := strings.Join(d, ".")
+	dir := strings.Replace(broker, ":", "_", 1)
+
+	return dir
+}
+
+func createProjBrokerDir(pdir, bdir string) {
+	path := path.Join(pdir, bdir)
+	err := os.Mkdir(path, 0700)
+	if err != nil {
+		displayGetError("error creating broker dir", err)
+	}
+	fmt.Printf("    created .disco/%s\n", bdir)
+
+	writeKeysTo(path)
+}
+
+func createRootBrokerDir(bdir string) {
+
+}
+
+func writeKeysTo(bdir string) {
+	pub, priv, kerr := crypt.CreateBrokerKeys()
+	if kerr != nil {
+		displayGetError("error creating broker keys", kerr)
+	}
+
+	public := writeKey("pub.key", pub, bdir)
+	if public {
+		fmt.Printf("    written .disco/%s/pub.key\n", filepath.Base(bdir))
+	}
+
+	private := writeKey("prv.key", priv, bdir)
+	if private {
+		fmt.Printf("    written .disco/%s/prv.key\n", filepath.Base(bdir))
+	}
+}
+
+func writeKey(name string, key []byte, dir string) bool {
+	path := path.Join(dir, name)
+	file, err := os.Create(path)
+	defer file.Close()
+
+	if err != nil {
+		displayGetError("error creating key file", err)
+		return false
+	}
+
+	_, werr := file.Write(key)
+	if werr != nil {
+		displayGetError("error writing key file", werr)
+		return false
+	}
+
+	return true
+}
+
+func displayGetError(msg string, err error) {
+	if err == nil {
+		fmt.Printf("disco get: %s\n", msg)
+	} else {
+		fmt.Printf("disco get: %s: %s\n", msg, err)
+	}
+	os.Exit(1)
 }
