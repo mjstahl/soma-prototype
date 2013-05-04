692acbfb4feeeb4abd97d860be6859a9afa4dd5b
Mark Stahl <mjstahl@gmail.com>
2013-05-04 13:15:14 -0400

    Worked on #75. Finished off the ast for integer. Time to move onto the behavior lookups.

--- src/soma/ast/integer.go
+++ src/soma/ast/integer.go
@@ -22,6 +22,7 @@ import (
 )
 
 type Integer struct {
+	Literal  string
 	Negative bool
 	Radix    int
 	Value    int64
@@ -37,17 +38,43 @@ func NewInteger(literal string) *Integer {
 	if len(parts) > 1 {
 		radix, _ := strconv.Atoi(parts[0])
 		mantissa, _ := strconv.ParseInt(parts[1], radix, 0)
-		return &Integer{negate, radix, mantissa}
+		return &Integer{literal, negate, radix, mantissa}
 	}
 
 	mantissa, _ := strconv.ParseInt(literal, 10, 0)
-	return &Integer{negate, 10, mantissa}
+	return &Integer{literal, negate, 10, mantissa}
 }
 
+// Methods to satisfy the rt.Expr interface
 func (i *Integer) Eval(s *rt.Scope) rt.Value {
+	if i.Value > (-1<<55) && i.Value < (1<<55-1) {
+		return i
+	}
 	return rt.NIL
 }
 
 func (i *Integer) Visit(s *rt.Scope) rt.Value {
 	return i.Eval(s)
+}
+
+// Methods to satisfy the rt.Value interface
+func (i *Integer) Address() rt.Mailbox {
+	return rt.INTEGER.Address()
+}
+
+func (i *Integer) LookupBehavior(name string) rt.Value {
+	return rt.INTEGER.LookupBehavior(name)
+}
+
+func (i *Integer) OID() uint64 {
+	return (uint64)((i.Value << 8) | ((int64)(i.Radix << 4)) | 0x7)
+}
+
+func (i *Integer) Return(am *rt.AsyncMsg) {
+	rt.INTEGER.Return(am)
+	return
+}
+
+func (i *Integer) String() string {
+	return i.Literal
 }
