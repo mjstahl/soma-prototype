8226336aeb15c10e857cafd8c62c399a8225e660
Mark Stahl <mjstahl@gmail.com>
2013-05-18 15:20:48 -0400

    Assignment now prints out the last expression evaluated on the left hand side. This will have to change when doing arrays.

--- src/soma/ast/assign.go
+++ src/soma/ast/assign.go
@@ -26,6 +26,7 @@ type Assign struct {
 
 func (a *Assign) Eval(s *rt.Scope) rt.Value {
 	lvar, lval := len(a.Targets), len(a.Exprs)
+	var v rt.Value
 	if lvar < lval {
 		for index, target := range a.Targets {
 			val := a.Exprs[index].Visit(s)
@@ -33,12 +34,13 @@ func (a *Assign) Eval(s *rt.Scope) rt.Value {
 		}
 
 		for _, expr := range a.Exprs[lvar:] {
-			expr.Visit(s)
+			v = expr.Visit(s)
 		}
 	} else {
 		for index, expr := range a.Exprs {
 			val := expr.Visit(s)
 			s.Insert(a.Targets[index], val.OID())
+			v = val
 		}
 
 		for _, target := range a.Targets[lval:] {
@@ -46,7 +48,7 @@ func (a *Assign) Eval(s *rt.Scope) rt.Value {
 		}
 	}
 
-	return rt.NIL
+	return v
 }
 
 func (a *Assign) Visit(s *rt.Scope) rt.Value {
