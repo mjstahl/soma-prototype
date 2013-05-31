6c909fa0c5c4b5cc2b75b694833d9d0e905c206a
Mark Stahl <mjstahl@gmail.com>
2013-05-31 12:21:22 -0400

    Changed the project to fall under the MIT and BSD-style licenses as opposed to AGPLv3. Removed copyright headers from each file.

--- src/soma/parse/interface.go
+++ src/soma/parse/interface.go
@@ -1,8 +1,3 @@
-// Copyright 2013 Mark Stahl. All rights reserved.
-// Copyright 2012 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the BSD-LICENSE file.
-
 package parse
 
 import (
--- src/soma/scan/scanner.go
+++ src/soma/scan/scanner.go
@@ -1,8 +1,3 @@
-// Copyright 2013 Mark Stahl. All rights reserved.
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the BSD-LICENSE file.
-
 package scan
 
 import (
