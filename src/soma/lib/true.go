c9e0669ddf8cd1377ba096bca533bc8c64f2bebb
Mark Stahl <mjstahl@gmail.com>
2014-03-07 09:31:54 -0600

    Updated function documents to state the correct syntax

--- src/soma/lib/true.go
+++ src/soma/lib/true.go
@@ -18,54 +18,63 @@ var trueBehaviorMap = map[string]primitiveFn{
 	"^":               trueXor,
 }
 
-// + True isNotNil => { True }
+// (True) isNotNil => True.
+//
 func trueIsNotNil(msg *rt.AsyncMsg) {
 	returnTrue(msg)
 }
 
-// + True isNil => { False }
+// (True) isNil => False.
+//
 func trueIsNil(msg *rt.AsyncMsg) {
 	returnFalse(msg)
 }
 
-// + True ifNil: nBlock => { Nil }
+// (True) ifNil: nBlock => Nil.
+//
 func trueIfNil(msg *rt.AsyncMsg) {
 	returnNil(msg)
 }
 
-// + True ifNotNil: nBlock => { nBlock value }
+// (True) ifNotNil: nBlock => nBlock value.
+//
 func trueIfNotNil(msg *rt.AsyncMsg) {
 	returnArgEval(msg, 1)
 }
 
-// + True ifFalse: fBlock => { Nil }
+// (True) ifFalse: fBlock => Nil.
+//
 func trueIfFalse(msg *rt.AsyncMsg) {
 	returnNil(msg)
 }
 
-// + True ifTrue: tBlock => { tBlock value }
+// (True) ifTrue: tBlock => tBlock value.
+//
 func trueIfTrue(msg *rt.AsyncMsg) {
 	returnArgEval(msg, 1)
 }
 
-// + True ifTrue: tBlock ifFalse: fBlock => { tBlock value }
+// (True) ifTrue: tBlock ifFalse: fBlock => tBlock value.
+//
 func trueIfTrueIfFalse(msg *rt.AsyncMsg) {
 	returnArgEval(msg, 1)
 }
 
-// + True not => { False }
+// (True) not => False.
+//
 func trueNot(msg *rt.AsyncMsg) {
 	returnFalse(msg)
 }
 
-// + True | aBool => { True }
+// (True) | aBool => True.
+//
 func trueOr(msg *rt.AsyncMsg) {
 	returnTrue(msg)
 }
 
-// + True & aBool => {
-//   aBool ifTrue: { True } ifFalse { False }
-// }
+// (True) & aBool =>
+//   aBool ifTrue: { True } ifFalse { False }.
+//
 func trueAnd(msg *rt.AsyncMsg) {
 	switch msg.Args[1] {
 	case rt.TRUE.OID():
@@ -75,9 +84,9 @@ func trueAnd(msg *rt.AsyncMsg) {
 	}
 }
 
-// + True ^ aBool => {
-//   aBool ifTrue: { False } ifFalse { True }
-// }
+// (True) ^ aBool =>
+//   aBool ifTrue: { False } ifFalse { True }.
+//
 func trueXor(msg *rt.AsyncMsg) {
 	switch msg.Args[1] {
 	case rt.TRUE.OID():
